5841bb8e59516a84587caeec586691690ddb3216
JJW <jason.wright.2007@gmail.com>
2019-01-22 16:23:26 -0600

    UMCOC advert in profile

--- account/account.py
+++ account/account.py
@@ -88,7 +88,7 @@ class Account:
                     data = discord.Embed(colour=discord.Color.gold())
                 data.add_field(name="Error:warning:",value="{} doesn't have an account at the moment, sorry.".format(user.mention))
 
-            data.set_footer(text='CollectorDevTeam - customize with /account update',
+            data.set_footer(text='CollectorDevTeam - customize with /account update\nJoin the UMCOC Community: https://discord.gg/umcoc',
                     icon_url=self.COLLECTOR_ICON)
             await self.bot.say(embed=data)
 
