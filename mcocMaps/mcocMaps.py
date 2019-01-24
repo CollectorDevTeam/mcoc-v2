e61a3b6aeb72bed3a0dc1a1559377b481f104c80
JJW <jason.wright.2007@gmail.com>
2019-01-23 20:56:03 -0600

    converter

--- mcocMaps/mcocMaps.py
+++ mcocMaps/mcocMaps.py
@@ -686,7 +686,7 @@ class MCOCMaps:
             champ[2:-2], #name
             champ[-1] # rank
         )
-        champion = ChampConverter(name)
+        champion = ChampConverter(self.bot, name)
         return champion
 
     # async def jm_parse_champ_filter(self, champ_filter):
