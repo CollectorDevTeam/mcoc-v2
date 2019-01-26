b48b290ef6f66d9d65453676f562e43fe3afb99b
JJW <jason.wright.2007@gmail.com>
2019-01-26 14:04:19 -0600

    indent

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -1034,31 +1034,31 @@ class CDTHelperFunctions:
 
     def tabulate_data(table_data, width=None, align=None, rotate=False, separate_header=True):
     '''Turn a list of lists into a tabular string'''
-    print('tabulate_data')
-    align_opts = {'center': '^', 'left': '<', 'right': '>'}
-    default_align = 'center'
-    default_width = 5
+        print('tabulate_data')
+        align_opts = {'center': '^', 'left': '<', 'right': '>'}
+        default_align = 'center'
+        default_width = 5
 
-    rows = []
-    if table_data:
-        tbl_cols = len(table_data[0])
-        if any(len(x) != tbl_cols for x in table_data):
-            raise IndexError('Array is not uniform')
+        rows = []
+        if table_data:
+            tbl_cols = len(table_data[0])
+            if any(len(x) != tbl_cols for x in table_data):
+                raise IndexError('Array is not uniform')
 
-        width = pad_list(width, tbl_cols, default_width)
-        align = pad_list(align, tbl_cols, default_align)
-        print(width)
-        print(align)
-        for i in iter_rows(table_data, rotate):
+            width = pad_list(width, tbl_cols, default_width)
+            align = pad_list(align, tbl_cols, default_align)
+            print(width)
+            print(align)
+            for i in iter_rows(table_data, rotate):
 
-            fstr = '{:{}{}}'.format(i[0], align_opts[align[0]], width[0])
-            if tbl_cols > 1:
-                for n in range(1, tbl_cols):
-                    fstr += '|{:{}{}}'.format(i[n], align_opts[align[n]], width[n])
-            rows.append(fstr)
-        if separate_header:
-            rows.insert(1, '-' * (sum(width) + len(width)))
-    return '\n'.join(rows)
+                fstr = '{:{}{}}'.format(i[0], align_opts[align[0]], width[0])
+                if tbl_cols > 1:
+                    for n in range(1, tbl_cols):
+                        fstr += '|{:{}{}}'.format(i[n], align_opts[align[n]], width[n])
+                rows.append(fstr)
+            if separate_header:
+                rows.insert(1, '-' * (sum(width) + len(width)))
+        return '\n'.join(rows)
 
 
     def pad_list(lst, new_length, pad_value):
