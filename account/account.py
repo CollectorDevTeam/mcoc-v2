06f887e6fe948ba13ac93cb3545153fb0b256d4a
JJW <jason.wright.2007@gmail.com>
2019-01-28 18:30:56 -0600

    turn off delete

--- account/account.py
+++ account/account.py
@@ -73,17 +73,18 @@ class Account:
             # pages.append(data)
             await PagesMenu.menu_start(self, [data])
 
-    @account.commands(pass_context=True, name="delete", invoke_without_command=True)
-    async def _delete(self, ctx):
-        '''Delete your CollectorVerse account'''
-        user = ctx.message.author
-        question= 'Are you sure you want to delete your CollectorVerse account?'
-        answer = PagesMenu.confirm(self, ctx, question)
-        if answer:
-            if user.id in self.nerdie:
-                dropped = self.nerdie.pop(user.id)
-                dataIO.save_json(self.profile, self.nerdie)
-            data.add_field(name="Congrats!:sparkles:", value="You have deleted your CollectorVerse account.")
+    # @_account.commands(pass_context=True, name="delete", invoke_without_command=True)
+    # async def _delete(self, ctx):
+    #     '''Delete your CollectorVerse account'''
+    #     user = ctx.message.author
+    #     question = 'Are you sure you want to delete your CollectorVerse account {}?'.format(user.name)
+    #     answer = PagesMenu.confirm(self, ctx, question)
+    #     if answer:
+    #         if user.id in self.nerdie:
+    #             dropped = self.nerdie.pop(user.id)
+    #             dataIO.save_json(self.profile, self.nerdie)
+    #         data.add_field(name="Congrats!:sparkles:", value="You have deleted your CollectorVerse account.")
+    #         await PagesMenu.menu_start(self, [data])
 
 
     # @commands.group(name="update", pass_context=True, invoke_without_command=True)
