80e43d7b6efc407039cb50779e0f2b9966968445
JJW <jason.wright.2007@gmail.com>
2019-02-01 16:27:49 -0600

    comments

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -1019,11 +1019,11 @@ class MCOCTools:
     #     event = 'eq_'
     #     await self.format_eventquest(event, tier.lower())
 
-    @commands.command(name='variant', pass_context=True)
+    @commands.command(name='variant', pass_context=True, hidden=True)
     async def eq_variant(self, ctx, chapter: str):
         '''Variant Quest
         chapters = 1.1, 1.2, 1.3, 2.1, 2.2, 2.3, 3.1, 3.2, 3.3'''
-        chapters = ('1.1', '1.2', '1.3', ' 2.1', '2.2', '2.3', '3.1', '3.2', '3.3',)
+        chapters = ('1.1', '1.2', '1.3', ' 2.1', '2.2', '2.3', '3.1', '3.2', '3.3','1MVP','2MVP','3MVP')
         if chapter not in chapters:
             return
         # valid = ('1.1', '1.2', '1.3', '1MVP','1Options', '2.1', '2.2', '2.3', '2MVP', '2Options', '3.1', '3.2', '3.3','3MVP','3Options',)
@@ -1031,16 +1031,11 @@ class MCOCTools:
         sgd=StaticGameData()
         vq = await sgd.get_gsheets_data('variant')
         # valid = vq.keys()
-        valid = ['1MVP','1Options','1.1A','1.1B','1.1Boss','1.1C','1.1D','1.1E','1.1F',
-                '1.2A','1.2B','1.2Boss','1.2C','1.2D','1.2E',
-                '1.3A','1.3B','1.3Boss','1.3C','1.3D','1.3E',
-                '2MVP','2Options','2.1A','2.1B','2.1Boss','2.1C','2.1D','2.1E','2.1F',
-                '2.2A','2.2B','2.2Boss','2.2C','2.2D',
-                '2.3A','2.3B','2.3Boss','2.3C','2.3D','2.3E','2.3F',
-                '3MVP','3Options','3.1A','3.1B','3.1Boss','3.1C','3.1D','3.1E','3.1F',
-                '3.2A','3.2B','3.2Boss','3.2C','3.2D','3.2E','3.2F',
-                '3.3A','3.3B','3.3Boss','3.3C','3.3D','3.3E','3.3F']
-        page_number = valid.index(chapter+'A')
+        valid = ['1MVP','1Options','1.1A','1.1B','1.1C','1.1D','1.1E','1.1F','1.1Boss','1.2A','1.2B','1.2C','1.2D','1.2E','1.2Boss','1.3A','1.3B','1.3C','1.3D','1.3E','1.3Boss','2MVP','2Options','2.1A','2.1B','2.1C','2.1D','2.1E','2.1F','2.1Boss','2.2A','2.2B','2.2C','2.2D','2.2Boss','2.3A','2.3B','2.3C','2.3D','2.3E','2.3F','2.3Boss','3MVP','3Options','3.1A','3.1B','3.1C','3.1D','3.1E','3.1F','3.1Boss','3.2A','3.2B','3.2C','3.2D','3.2E','3.2F','3.2Boss','3.3A','3.3B','3.3C','3.3D','3.3E','3.3F','3.3Boss']
+        if chapter in valid:
+            page_number = valid.index(chapter)
+        else:
+            page_number = valid.index(chapter+'A')
         page_list = []
         for cp in valid:
             v = vq[cp]
@@ -1053,8 +1048,8 @@ class MCOCTools:
                 data.add_field(name='Fights', value=v['fights'])
             if 'boosts' in v:
                 data.add_field(name='Boosts', value=v['boosts'])
-            # if 'comments' in v:
-            #     data.add_field(name='Comments', value=v['comments'])
+            if 'comments' in v:
+                data.add_field(name='Comments', value=v['comments'])
 
             page_list.append(data)
         menu = PagesMenu(self.bot, timeout=120, delete_onX=True, add_pageof=True)
