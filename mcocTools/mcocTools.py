24f16cfd9c8ca6df6284dfb9afd25ee0404e9310
JJW <jason.wright.2007@gmail.com>
2019-02-04 16:30:40 -0600

    whitespace in 2.1

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -1022,21 +1022,21 @@ class MCOCTools:
     @commands.command(name='variant', pass_context=True, hidden=False)
     async def eq_variant(self, ctx, chapter: str):
         '''Variant Quest
-        1.1 : Dark Portents (Power Reserve)
-        1.2 : A Villain Revealed (Burden of Might)
-        1.3 : The Collector's Task (Masochism & Aspect of Chaos)
+        1.1 : Dark Portents
+        1.2 : A Villain Revealed
+        1.3 : The Collector's Task
         2.1 : Ultron Strikes
-        2.2 : Drone Duel (Masochism)
+        2.2 : Drone Duel
         2.3 : The Avengers
-        3.1 : The Campaign (Masochism Global)
-        3.2 : Heroes Arise (Pilfer Global)
-        3.3 : Avengers Assemble (Fisticuffs Global - no de/buffs for first 10s)
+        3.1 : The Campaign
+        3.2 : Heroes Arise
+        3.3 : Avengers Assemble
         '''
 
 
         sgd=StaticGameData()
         vq = await sgd.get_gsheets_data('variant')
-        chapters = ('1.1', '1.2', '1.3', ' 2.1', '2.2', '2.3', '3.1', '3.2', '3.3')
+        chapters = ('1.1', '1.2', '1.3', '2.1', '2.2', '2.3', '3.1', '3.2', '3.3')
         valid = ['1.1A','1.1B','1.1C','1.1D','1.1E','1.1F','1.1Boss','1.2A','1.2B','1.2C','1.2D','1.2E','1.2Boss','1.3A','1.3B','1.3C','1.3D','1.3E','1.3Boss','2.1A','2.1B','2.1C','2.1D','2.1E','2.1F','2.1Boss','2.2A','2.2B','2.2C','2.2D','2.2Boss','2.3A','2.3B','2.3C','2.3D','2.3E','2.3F','2.3Boss','3.1A','3.1B','3.1C','3.1D','3.1E','3.1F','3.1Boss','3.2A','3.2B','3.2C','3.2D','3.2E','3.2F','3.2Boss','3.3A','3.3B','3.3C','3.3D','3.3E','3.3F','3.3Boss']
         if chapter not in chapters and chapter not in valid:
             return
