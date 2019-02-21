36b0f2ff3048dbd0fcc2bd1473139988cb69a644
JJW <jason.wright.2007@gmail.com>
2019-02-21 16:01:42 -0600

    sparkles

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -735,8 +735,10 @@ class MCOCTools:
         topic = channel.topic
         if topic is not None and topic != '':
             data = discord.Embed(color=ctx.message.author.color,
-                                 title='{} Topic :sparkles:'.format(ctx.message.channel.name.title()),
+                                 title='#{} Topic :sparkles:'.format(ctx.message.channel.name),
                                  description=topic)
+            data.thumbnail(url=ctx.message.server.icon_url)
+            data.set_footer(text='CollectorDevTeam', icon_url=self.COLLECTOR_ICON)
             await self.bot.say(embed=data)
 
     @commands.command(pass_context=True, aliases={'collector', 'infocollector', 'about'})
