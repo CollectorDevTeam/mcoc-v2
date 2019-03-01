e058f912bcebefe35dccd93004f23cd67471a5c1
JJW <jason.wright.2007@gmail.com>
2019-02-28 20:01:46 -0600

    append the page at the right spot

--- hook/hook.py
+++ hook/hook.py
@@ -559,7 +559,7 @@ class Hook:
                                     .format(klass, count, power, percent))
                         if len(list) > 0:
                             data.add_field(name='{0}★ {1}'.format(star, klass), value='\n'.join(list))
-                            pages.append(data)
+                pages.append(data)
             menu = PagesMenu(self.bot, timeout=120, delete_onX=True, add_pageof=True)
             await menu.menu_start(pages=pages)
 
