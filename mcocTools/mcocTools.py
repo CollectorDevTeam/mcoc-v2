c882f2418898e57433079a457c22ce602685a3c4
JJW <jason.wright.2007@gmail.com>
2019-04-05 12:43:17 -0500

    style a bit

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -928,16 +928,16 @@ class MCOCTools:
             description = []
             for k in range(1, 23):
                 k = str(k)
-                if '5feature' in self.cutoffs[k] and self.cutoffs[k]['5feature'] == champ.full_name:
+                if '5feature' in self.cutoffs[k] and self.cutoffs[k]['feature'] == champ.full_name:
                     description.append(
-                        '{} [F] {} | 5★ {}\n'.format(self.cutoffs[k]['arena_date'], self.cutoffs[k]['5feature'],
+                        '{} [F5★] {} | {}\n'.format(self.cutoffs[k]['arena_date'], self.cutoffs[k]['5feature'],
                                                      self.cutoffs[k]['feature']))
-                if '4feature' in self.cutoffs[k] and self.cutoffs[k]['4feature'] == champ.full_name:
+                if '4feature' in self.cutoffs[k] and self.cutoffs[k]['feature'] == champ.full_name:
                     description.append(
-                        '{} [F] {} | 4★ {}\n'.format(self.cutoffs[k]['arena_date'], self.cutoffs[k]['4feature'],
+                        '{} [F4★] {} | {}\n'.format(self.cutoffs[k]['arena_date'], self.cutoffs[k]['4feature'],
                                                      self.cutoffs[k]['feature']))
-                if '4basic' in self.cutoffs[k] and self.cutoffs[k]['4basic'] == champ.full_name:
-                    description.append('{} [B] {} | 4★ {}\n'.format(self.cutoffs[k]['arena_date'], self.cutoffs[k]['4basic'],
+                if '4basic' in self.cutoffs[k] and self.cutoffs[k]['basic'] == champ.full_name:
+                    description.append('{} [B4★] {} {}\n'.format(self.cutoffs[k]['arena_date'], self.cutoffs[k]['4basic'],
                                                                     self.cutoffs[k]['basic']))
             description = ''.join(description)
             arena_pages = chat.pagify(description, page_length=500)
