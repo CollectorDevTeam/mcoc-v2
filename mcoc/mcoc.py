95252170b9c7bf5de0fcdce17f25d2718ab0ca9c
JJW <jason.wright.2007@gmail.com>
2019-05-02 19:58:07 -0500

    final tldr

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1024,12 +1024,12 @@ class MCOC(ChampionFactory):
                 uid = 'user{}'.format(i)
                 tid = 'tldr{}'.format(i)
                 if uid in tldr[k]:
-                    data.add_field(name='{} says:'.format(tldr[k][uid]), value=tldr[k][tid])
+                    data.add_field(name='{} says:'.format(tldr[k][uid]), value=tldr[k][tid], inline=False)
             if 4 not in tldr[k]:
                 data.description = 'Don\'t like that advice?  \nAdd a TLDR here: [TLDR Form](https://forms.gle/EuhWXyE5kxydzFGK8)'
         else:
             data.description = 'No information.  \nAdd a TLDR here: [TLDR Form](https://forms.gle/EuhWXyE5kxydzFGK8)'
-        data.add_field(name='Shortcode', value=champ.short)
+        data.add_field(name='Shortcode', value=champ.short, inline=False)
         data.set_footer(text='CollectorDevTeam Dataset', icon_url=COLLECTOR_ICON)
         data.set_thumbnail(url=champ.get_avatar())
         await self.bot.say(embed=data)
