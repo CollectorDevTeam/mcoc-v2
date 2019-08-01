97f4b23415adaa5c8546d0b12b84c71ae0c06431
JJW <jason.wright.2007@gmail.com>
2019-08-01 18:12:59 -0500

    manually add a few patrons

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -1021,6 +1021,9 @@ class MCOCTools:
         partners = ', '.join(self._get_members(cdt, cdtpartners))
         if self._get_members(cdt, cdtpatrons) is not None:
             patrons = ', '.join(self._get_members(cdt, cdtpatrons))
+            patrons.append('Caitibee')
+            patrons.append('Fabio F.')
+            patrons.append('Vajda R.')
         else:
             patrons = "[Become a CollectorBot patron today!](https://patreon.com/collectorbot)"
 
