bd4280e1de6b1fa34a2730684b4a44177450b802
JJW <jason.wright.2007@gmail.com>
2019-12-02 12:34:44 -0600

    remove self.bot

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -2208,7 +2208,7 @@ class CDTGAPS:
         await self.bot.delete_message(message2)
         try:
             alliance = self.bot.get_cog("Alliance")
-            await alliance.register_alliance(self.bot, ctx)
+            await alliance.register_alliance(ctx)
             return
         except:
             raise
