3edd3de0e674f933e74f7c17f0aec07b0f96db11
Jason <jason@sdicgm.com>
2020-06-12 14:25:02 -0500

    self

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -800,7 +800,7 @@ class MCOC(ChampionFactory):
         #print(len(sgd.cdt_data), len(sgd.cdt_masteries), sgd.test)
         cm = sgd.cdt_masteries
         if cm is None:
-            await StaticGameData.load_cdt_data()
+            await StaticGameData.load_cdt_data(self)
             cm = StaticGameData.cdt_masteries
         found = False
         page_list = []
